cd61e8d6d2ce26bee99c6a3ae4cca243f4e27efe
Fukusan64 <ikishikawa64@gmail.com>
2019-03-07 04:16:22 +0900

    とりあえず立方体が出るようにした

---
+++ src/components/Background.vue
@@ -0,0 +1,70 @@
+<template>
+  <canvas id="BGCanvas" ref="BGCanvas"></canvas>
+</template>
+
+<script>
+
+import * as THREE from 'three';
+import OrbitControls from 'three-orbitcontrols';
+import * as TWEEN from '@tweenjs/tween.js';
+
+export default {
+  props: ['isActive'],
+  data() {
+    return {
+      camera: null,
+      renderer: null,
+      scene: null,
+      tree: null,
+      light: null,
+      control: null,
+    };
+  },
+  mounted() {
+    const canvas = this.$refs.BGCanvas;
+    const [width, height] = [canvas.clientWidth, canvas.clientHeight];
+    this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+    this.renderer.setSize(width, height);
+
+    this.scene = new THREE.Scene();
+
+    this.light = new THREE.AmbientLight(0xFFFFFF, 1.0);
+    this.scene.add(this.light);
+
+    this.camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
+    this.camera.position.set(0, 0, 100);
+    this.scene.add(this.camera);
+
+    this.tree = new THREE.Mesh(
+      new THREE.BoxGeometry(50, 50, 50),
+      new THREE.MeshPhongMaterial({color: 0x66B6FF,specular: 0xffffff})
+    );
+    this.tree.position.set(0, 0, 0);
+    this.scene.add(this.tree);
+
+    this.control = new OrbitControls(this.camera);
+
+    this.animate();
+  },
+  methods: {
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.control.update();
+      this.renderer.render(this.scene, this.camera);
+    }
+  },
+};
+</script>
+
+<style scoped>
+  #BGCanvas{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    z-index: -1;
+  }
+</style>
