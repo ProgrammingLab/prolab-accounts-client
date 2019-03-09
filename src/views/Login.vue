47e7a6c0f71b198334cba0059eb9aa070fbac427
Fukusan64 <ikishikawa64@gmail.com>
2019-03-09 23:41:54 +0900

    見た目を oauth/login と同じにした

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,14 @@
 <template>
-  <div id="background">
-    <div id="container">
-      <img width="200" height="70" src="http://placehold.jp/60/ffffff/666666/200x70.png?text=ProLab" alt="prolag">
-      <form @submit.prevent="onLogin">
-        <label>
-          Username
-          <input type="text" autocomplete="username" v-model="name"/>
-        </label>
-        <label>
-          Password
-          <input type="password" autocomplete="current-password" v-model="password"/>
-        </label>
+  <div class="container">
+    <div class="box">
+      <img class="logo" src="../assets/logo.svg" alt="ProLab">
+      <form v-on:submit.prevent="onLogin">
+        <label for="id">Name or Email Address</label>
+        <input required type="text" v-model="name">
+        <label for="password">Password</label>
+        <input required type="password" v-model="password">
+        <button type="submit">Log in</button>
         <ErrorMessage :error="loginError"/>
-        <button type="submit" :disabled="!password || !name">Login</button>
       </form>
     </div>
   </div>
@@ -56,44 +52,42 @@ export default {
 </script>
 
 <style scoped>
-  #background {
+  .container {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 100%;
+    flex-direction: column;
     height: 100%;
   }
-  #container {
-    width: 50%;
-    height: 50%;
-  }
-  form {
-    margin-top: 1em;
-  }
-  input {
-    outline: none;
-    padding-top: 0.5em;
-    width: 100%;
-    border: none;
-    border-bottom: 2px solid #888888;
-    background: transparent;
-  }
-  input:focus {
-    border-bottom: 2px solid #333333;
-  }
-  button {
-    float: right;
-    margin-top: 0.5em;
-    background-color: transparent;
-    border: 1px solid;
-    outline: none;
-    color: #333333;
-  }
-  button:disabled {
-    color: #888888;
-  }
-  label {
-    width: 100%;
+
+  img {
+    width: 40%;
+    margin-bottom: 20px;
   }
 
+  .box {
+    margin: 0 2em;
+  }
+
+  input[type="text"], input[type="password"] {
+    width: 100%;
+    padding: 10px;
+    border-width: 0 0 2px;
+    margin-bottom: 20px;
+  }
+
+  button[type="submit"] {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    padding: 0;
+    appearance: none;
+    display: inline-block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #668ad8;
+    color: #FFF;
+    border-radius: 3px;
+  }
 </style>
