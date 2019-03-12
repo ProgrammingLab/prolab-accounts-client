0ecbf16465cd8ff9a8eb85a8d40e74e01af004d1
Wister <fujianifan@gmail.com>
2019-03-12 11:32:30 +0900

    EditProfile.vueの項目をstyle側で太字にするようにした

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -3,21 +3,21 @@
         <img width="210" height="70" src="http://placehold.jp/65/ffffff/666666/210x70.png?text=ProLab" alt="prolag">
         <!-- <img alt="Prolab Logo" src="../assets/logo.png"> -->
         <h1>プロフィール編集ページ</h1>
-        <b id="uname">user: prolab_user</b>
+        <p id="uname" class="profileItem">user: prolab_user</p>
         <form>
             <input type="checkbox" name="profile_publish" id="checkbox" value="enabled">
             プロフィールを公開する<br>
-            <b>表示名</b><br>
+            <label for="display_name" class="profileItem">表示名</label><br>
             <input type="text" name="display_name" value="user"><br>
-            <b>本名</b><br>
+            <label for="real_name" class="profileItem">本名</label><br>
             <input type="text" name="real_name" value="Real User"><br>
-            <b>学年</b><br>
+            <label for="grade" class="profileItem">学年</label><br>
             <input type="text" name="grade" value="2"><br>
-            <b>学科</b><br>
+            <label for="department" class="profileItem">学科</label><br>
             <input type="text" name="department" value="S"><br>
-            <b>Twitter ID</b><br>
+            <label for="display_name" class="profileItem">Twitter ID</label><br>
             <input type="text" name="Twitter" value="@TwitterUser"><br>
-            <b>Github Name</b><br>
+            <label for="display_name" class="profileItem">Github Name</label><br>
             <input type="text" name="Github" value="GithubUser"><br>
             <input type="submit" value="保存する">
         </form>
@@ -25,12 +25,15 @@
 </template>
 
 <style scoped>
-    #a{
+    #page{
         margin-left: 10vw;
         margin-right: 10vw;
         color:#666666;
     }
-    b#uname{
+    #uname{
         font-size: 25px;
+    }
+    .profileItem{
+        font-weight: bold;
     }
 </style>
