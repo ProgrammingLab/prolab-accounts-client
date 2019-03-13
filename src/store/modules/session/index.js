0dcc16d334dad9d0b44347dddcad2f142da5082b
Fukusan64 <ikishikawa64@gmail.com>
2019-03-13 18:47:48 +0900

    loggedInを常にbool値を返すようにした

--- src/store/modules/session/index.js
+++ src/store/modules/session/index.js
@@ -29,7 +29,7 @@ export default {
   },
   getters: {
     loggedIn({ sessionID, loginError }) {
-      return sessionID && !loginError;
+      return Boolean(sessionID && !loginError);
     },
   },
 };
