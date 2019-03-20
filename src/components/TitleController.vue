11ae1864364750f0a11fa09d7a5353e10e435b25
Fukusan64 <ikishikawa64@gmail.com>
2019-03-21 00:38:13 +0900

    コンポーネントを作った

---
+++ src/components/TitleController.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'titleController',
+  props:['title'],
+  mounted() {
+    document.title = this.title;
+  },
+  watch: {
+    title(title) {
+      document.title = title;
+    }
+  }
+};
+</script>
