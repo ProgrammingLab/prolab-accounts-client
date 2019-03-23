f0d0787609259bce466ce25be3cee03a6c5b44b2
Fukusan64 <ikishikawa64@gmail.com>
2019-03-23 23:46:09 +0900

    user data を取得するapiを叩く部分を書いた

---
+++ src/api/user/index.js
@@ -0,0 +1,9 @@
+import api from '@/api';
+import getHeader from '../utils/get-header';
+
+export default {
+  async getUser(sessionID) {
+    const userData = await api.client.get('/user', getHeader(sessionID));
+    return userData;
+  },
+};
