2e93ff1d92a4847fe26eecf8c0379a08a81cbed5
Fukusan64 <ikishikawa64@gmail.com>
2019-03-24 01:48:51 +0900

    いらない情報を削るようにした

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -3,7 +3,7 @@ import getHeader from '../utils/get-header';
 
 export default {
   async getUser(sessionID) {
-    const userData = await api.client.get('/user', getHeader(sessionID));
+    const userData = (await api.client.get('/user', getHeader(sessionID))).data;
     return userData;
   },
 };
