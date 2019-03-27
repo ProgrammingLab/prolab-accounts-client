74eb27926977c5f0cfa3573ec43441bc7263da2c
kp54 <kangpang65@gmail.com>
2019-03-27 18:09:04 +0900

    ロジックを書いた

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -7,9 +7,51 @@
                 <label for="full_name">Full Name</label>
                 <input required type="text" v-model="full_name"></input>
                 <label for="password"></label>
-                <input required type="password" v-model=""></input>
+                <input required type="password" v-model="password"></input>
                 <button type="submit">Register</button>
+                <ErrorMessage :error="registrationError"/>
             </form>
         </div>
     </div>
 </template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+import ErrorMessage from '@/components/ErrorMessage.vue';
+
+export default {
+    name: 'registration',
+    data() {
+        return {
+            name: '',
+            full_name: '',
+            password: '',
+            registration_token: '',
+        };
+    },
+    computed: {
+        ...mapState('user', [
+            'registrationError'
+        ]),
+    },
+    mounted() {
+        this.registration_token = this.$route.params.token
+    },
+    methods: {
+        ...mapActions('user', [
+            'register',
+        ]),
+        async onRegister() {
+            await this.register({
+                name: this.name,
+                full_name: this.full_name,
+                password: this.password,
+                registration_token: this.registration_token,
+            });
+            if (! this.registrationError) {
+                this.$router.push({name: 'login'});
+            }
+        },
+    },
+};
+</script>
