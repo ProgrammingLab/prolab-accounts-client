ea4ece584f8cce198851864206fa9cbd6de05a01
kp54 <kangpang65@gmail.com>
2019-03-27 18:58:21 +0900

    api書いた

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -6,4 +6,8 @@ export default {
     const userData = (await api.client.get('/user', getHeader(sessionID))).data;
     return userData;
   },
+  async registerUser(name, full_name, password, registration_token) {
+    const ret = (await api.client.post('/user', {name, full_name, password, registration_token})).data;
+    return ret;
+  },
 };
