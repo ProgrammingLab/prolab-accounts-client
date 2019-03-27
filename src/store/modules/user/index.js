15df5c97260995c2d14b162ab602664679b2e6e0
kp54 <kangpang65@gmail.com>
2019-03-27 18:27:07 +0900

    store書いた

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -4,17 +4,32 @@ export default {
   namespaced: true,
   state: {
     userData: null,
+    registrationError: null,
   },
   /* eslint-disable no-param-reassign */
   mutations: {
     setUser(state, user) {
       state.userData = user;
     },
+    setRegistrationError(state, error) {
+      state.registrationError = error;
+    },
+    clearRegistrationError(state) {
+      state.registrationError = null;
+    },
   },
   /* eslint-enable no-param-reassign */
   actions: {
     async getUser({ commit }, sessionID) {
       commit('setUser', await userClient.getUser(sessionID));
+    },
+    async register({ commit }, { name, full_name, password, registration_token }) {
+      commit('clearRegistrationError');
+      try {
+        await userClient.registerUser(name, full_name, password, registration_token);
+      } catch (e) {
+        commit('setRegistrationError', e);
+      }
     },
   },
 };
