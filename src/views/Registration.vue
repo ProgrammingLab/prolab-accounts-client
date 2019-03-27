1cabdf5eb9a5c824a2dca0d666683135ad5165e8
kp54 <kangpang65@gmail.com>
2019-03-27 16:22:54 +0900

    テンプレート書いた

---
+++ src/views/Registration.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="box">
+            <form action='post'>
+                <label for="username">User Name</label>
+                <input required type="text" v-model="name"><input>
+                <label for="full_name">Full Name</label>
+                <input required type="text" v-model="full_name"></input>
+                <label for="password"></label>
+                <input required type="password" v-model=""></input>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    </div>
+</template>
