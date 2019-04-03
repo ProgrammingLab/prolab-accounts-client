5acace664da3b88853b944747c40f48cddc085b5
kp54 <kangpang65@gmail.com>
2019-04-04 02:30:51 +0900

    骨組み

---
+++ src/views/memberIntroduction/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ErrorMessage :error="error"/>
+    <dl>
+      <dt>Name</dt> <!-- Display Name -->
+      <dd></dd>
+
+      <dt>Icon</dt>
+      <dd></dd>
+
+      <dt>Department</dt>
+      <dd></dd>
+
+      <dt>Grade</dt> <!-- Left -->
+      <dd></dd>
+
+      <dt>Description</dt>
+      <dd></dd>
+
+      <dt>Role</dt>
+      <dd></dd>
+
+      <dt>Twitter</dt>
+      <dd></dd>
+
+      <dt>GitHub</dt>
+      <dd></dd>
+
+      <dt>AtCoder</dt>
+      <dd></dd>
+    </dl>
+  </div>
+</template>
