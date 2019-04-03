671a4da756e1fee1b9363d4b14391775fe4a1fe1
Fukusan64 <ikishikawa64@gmail.com>
2019-04-04 01:46:41 +0900

    fix bug

--- src/api/memberIntroduction/memberList.js
+++ src/api/memberIntroduction/memberList.js
@@ -2,7 +2,7 @@ import api from '@/api';
 
 export default {
   async getUserList(token) {
-    const res = (await api.get('users', { page_token: token })).data;
+    const res = (await api.client.get('/users', { page_token: token })).data;
     return res;
   },
 };
