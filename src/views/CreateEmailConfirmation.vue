496dcfdbe516336c3e3e4a8fa41b422598a5cf2c
kp54 <kangpang65@gmail.com>
2019-04-09 00:03:34 +0900

    view

---
+++ src/views/CreateEmailConfirmation.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <div>
+      <form v-on:submit.prevent="onCreateEmailConfirmation">
+        <label for="new_email">New Email</label>
+        <input required type="text" id="new_email" v-model="new_email">
+        <label for="password">Password</label>
+        <input required type="password" id="password" v-model="password">
+        <button type="submit">Confirm</button>
+      </form>
+      <ErrorMessage :error="emailConfirmationError">
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+import ErrorMessage from '@/components/ErrorMessage.vue';
+
+export default {
+  name: 'createEmailConfirmation',
+  components: {
+    ErrorMessage,
+  },
+  data() {
+    return {
+      new_email: '',
+      password: '',
+    };
+  },
+  computed: {
+    ...mapState('email_confirmations', [
+      'emailConfirmationError',
+    ]),
+    ...mapState('session', [
+      'sessionID',
+    ]),
+  },
+  methods: {
+    ...mapActions('email_confirmations', [
+      'CreateEmailConfirmation',
+    ]),
+    async onCreateEmailConfirmation() {
+      await this.CreateEmailConfirmation({
+        session: this.sessionID,
+        new_email: this.new_email,
+        password: this.password,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 1024px;
+  margin: auto;
+}
+
+.box {
+  margin: 0 2em;
+}
+
+input[type="text"], input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  border-width: 0 0 2px;
+  margin-bottom: 20px;
+}
+
+button[type="submit"] {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+  appearance: none;
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #668ad8;
+  color: #FFF;
+  border-radius: 3px;
+}
+</style>
