a6161ff8b2e611774958ac6070f70b063ca75edf
kp54 <kangpang65@gmail.com>
2019-04-12 00:26:41 +0900

    decoratorぽいことをするとよさそうだった

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,26 @@
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
+const ifprod = (...plugins) => process.env.NODE_ENV === 'development' ? plugins : [] ;
+
 module.exports = {
   configureWebpack: {
     plugins: [
-      new CompressionWebpackPlugin({
-        asset: '[path].br[query]',
-        algorithm: 'brotliCompress',
-        test: /\.(js|map|css|json|txt|html|ico|svg)(\?.*)?$/i,
-        compressionOptions: { level: 11 },
-        minRatio: 0.8,
-      }),
-      new CompressionWebpackPlugin({
-        asset: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: /\.(js|map|css|json|txt|html|ico|svg)(\?.*)?$/i,
-        compressionOptions: { level: 9 },
-        minRatio: 0.8,
-      }),
+      ...ifprod(
+        new CompressionWebpackPlugin({
+          asset: '[path].br[query]',
+          algorithm: 'brotliCompress',
+          test: /\.(js|map|css|json|txt|html|ico|svg)(\?.*)?$/i,
+          compressionOptions: { level: 11 },
+          minRatio: 0.8,
+        }),
+        new CompressionWebpackPlugin({
+          asset: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.(js|map|css|json|txt|html|ico|svg)(\?.*)?$/i,
+          compressionOptions: { level: 9 },
+          minRatio: 0.8,
+        }),
+      ),
     ],
   },
 };
