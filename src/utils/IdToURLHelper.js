9dc5c15f7c8c601ffe096bdecddd1d28144d6562
kp54 <kangpang65@gmail.com>
2019-04-12 11:38:56 +0900

    linterに怒られた

--- src/utils/IdToURLHelper.js
+++ src/utils/IdToURLHelper.js
@@ -1,5 +1,5 @@
 export default {
-  AtCoder: (username) => `https://atcoder.jp/users/${username}`,
-  GitHub: (username) => `https://github.com/${username}`,
-  Twitter: (screenname) => `https://twitter.com/${screenname}`,
+  AtCoder: username => `https://atcoder.jp/users/${username}`,
+  GitHub: username => `https://github.com/${username}`,
+  Twitter: screenname => `https://twitter.com/${screenname}`,
 };
