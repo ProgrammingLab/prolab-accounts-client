e7ba36b73da948f92c3baf7fcf0868d520779ebc
Ryota Egusa <egusa.ryota@gmail.com>
2019-04-13 11:30:33 +0900

    Delete unused css

--- src/components/AchievementsList.vue
+++ src/components/AchievementsList.vue
@@ -56,42 +56,6 @@ li:hover {
   transition-duration: 0.2s;
 }
 
-dl {
-  margin: 20px;
-}
-dt {
-  font-size: 1.4rem;
-}
-dd, dt {
-  width: 85%;
-}
-dd.award {
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-}
-dd.year {
-  text-align: right;
-  position: absolute;
-  right: 0;
-  top: 6px;
-  color: #ccc;
-  font-size: 3.5rem;
-  transform: rotate(-90deg) translateY(-100%);
-  transform-origin: right top;
-  line-height: 1;
-  font-family: 'Barlow';
-}
-li:hover dd.year {
-  color: white;
-}
-p {
-  text-overflow: ellipsis;
-}
-img {
-  width: 100%;
-}
-
 @media screen and (max-width: 1024px) {
   li {
     width: 48%;
