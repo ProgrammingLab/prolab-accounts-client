7c961cfeb1916b5105291f35d565e08a8d690057
Ryota Egusa <egusa.ryota@gmail.com>
2019-04-13 10:17:55 +0900

    Add achievement api client

--- src/api/achievement/index.js
+++ src/api/achievement/index.js
@@ -1,8 +1,21 @@
 import api from '@/api';
+import getHeader from '@/api/utils/get-header';
 
 export default {
   async getAchievements() {
     const achievementData = (await api.client.get('/achievements')).data;
     return achievementData;
+  },
+  async createAchievement(sessionID, achievement) {
+    const resp = (await api.client.post('/achievements'), achievement, getHeader(sessionID)).data;
+    return resp;
+  },
+  async updateAchievement(sessionID, achievement) {
+    const resp = (await api.client.post(`/achievements/${achievement.achievement_id}`), achievement, getHeader(sessionID)).data;
+    return resp;
+  },
+  async deleteAchievement(sessionID, achievementID) {
+    const resp = (await api.client.delete(`/achievements/${achievementID}`), getHeader(sessionID)).data;
+    return resp;
   },
 };
