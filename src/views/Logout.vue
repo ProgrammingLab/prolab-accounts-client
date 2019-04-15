659455b2b546b0fc4f2ae1824241c8771ef1f4bd
kp54 <kangpang65@gmail.com>
2019-04-15 22:16:59 +0900

    Logout.vue

---
+++ src/views/Logout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'logout',
+  metaInfo: {
+    title: 'ログアウト',
+  },
+  computed: {
+    ...mapState('session', [
+      'sessionID',
+    ]),
+  },
+  async created() {
+    await this.logout(this.sessionID);
+    this.$router.push({ name: 'home' });
+  },
+  methods: {
+    ...mapActions('session', [
+      'logout',
+    ]),
+  },
+};
+</script>
