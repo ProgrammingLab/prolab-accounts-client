5e1204a12e334f9a10a84ecc58eb558511f301fd
Fukusan64 <ikishikawa64@gmail.com>
2019-05-06 00:26:37 +0900

    ImgXコンポーネントを作って試しに使ってみた

---
+++ src/components/ImgX.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="src ? src : require('@/assets/no_image.png')" :srcset="srcset" :alt="alt">
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+    },
+    media: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    srcset() {
+      if (!this.src) return '';
+      return this.media.map(({ pixelRatio, size }) => `${this.src}_${size} ${pixelRatio}`).join(',');
+    },
+  },
+};
+</script>
